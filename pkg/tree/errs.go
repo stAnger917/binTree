c338eea56af75e01fbb9ea6ae937b342813a8fbb
stAnger917 <d.kott@yandex.ru>
2022-10-09 20:54:22 +0600

    deleted tree

--- pkg/tree/errs.go
+++
@@ -1,7 +0,0 @@
-package tree
-
-import "errors"
-
-var errTreeIsNil = errors.New("tree is nil")
-var errNodeAlreadyExist = errors.New("this node index already exists")
-var errValueNotFound = errors.New("no value with such index")
